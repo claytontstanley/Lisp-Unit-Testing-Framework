a9637ee90cb168c83895b77ee523e901e175a87c
clayton stanley <claytonstanley@clayton-stanleys-MacBook-Pro.local>
2010-09-06 14:56:27 -0400

    refactored to allign with lol.lisp g! and o! abstractions, and added *success* dynamic variable to keep track of the overall success of the unit tests

--- unitTestFramework.lisp
+++ unitTestFramework.lisp
@@ -1,32 +1,40 @@
 (defvar *test-name* nil)
+(defvar *success* t)
 
-(defmacro with-gensyms ((&rest names) &body body)
-  `(let ,(loop for n in names collect `(,n (gensym)))
-     ,@body))
-
+(defmacro runtests (&body tests)
+  `(let ((*success* t))
+     (combine-results ,@tests)
+     *success*))
+ 
 (defmacro deftest (name parameters &body body)
   "Define a test function. Within a test function we can call
    other test functions or use 'check' to run individual test
    cases."
   `(defun ,name ,parameters
-     (let ((*test-name* (append *test-name* (list ',name))))
-       ,@body)))
+    (let ((*test-name* (append *test-name* (list ',name)))
+	  (*success* t))
+      ,@body
+      *success*)))
 
 (defmacro check (&body forms)
   "Run each expression in 'forms' as a test case."
   `(combine-results
     ,@(loop for f in forms collect `(report-result ,f ',f))))
 
-(defmacro combine-results (&body forms)
+(defmacro! combine-results (&body forms)
   "Combine the results (as booleans) of evaluating 'forms' in order."
-  (with-gensyms (result)
-    `(let ((,result t))
-       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
-       ,result)))
+  `(let ((,g!result t))
+     ,@(loop for f in forms collect `(unless ,f (setf ,g!result nil)))
+     (unless ,g!result (setf *success* nil))
+     ,g!result))
 
-(defun report-result (result form)
+(defmacro! report-result (o!result form)
   "Report the results of a single test case. Called by 'check'."
-  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
-  result)
+  `(progn
+     (format t "~:[FAIL~;pass~] ... ~a: ~a~%" ,g!result *test-name* ,form)
+     ,g!result))
+
+
+
 
 
